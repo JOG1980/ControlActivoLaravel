ecb1e92f4ae076d82d36f5f75db0f4091142f3f2
Jorge Ortiz <jogv19@gmail.com>
2022-01-20 01:35:47 -0600

    Se generalizaron nombres de variables en EStadoRegistroSeeder.

--- database/seeders/EstadoRegistroSeeder.php
+++ database/seeders/EstadoRegistroSeeder.php
@@ -14,29 +14,29 @@ class EstadoRegistroSeeder extends Seeder
      */
     public function run()
     {
-        $estado_registro1 = new EstadoRegistro();
-        $estado_registro1->nombre ="Normal";
-        $estado_registro1->descripcion="Registro en condiciones normales";
-        $estado_registro1->save();
+        $registro1 = new EstadoRegistro();
+        $registro1->nombre ="Normal";
+        $registro1->descripcion="Registro en condiciones normales";
+        $registro1->save();
 
-        $estado_registro2 = new EstadoRegistro();
-        $estado_registro2->nombre ="Pendiente";
-        $estado_registro2->descripcion="Registro pendiente, inconcluso, etc.";
-        $estado_registro2->save();
+        $registro2 = new EstadoRegistro();
+        $registro2->nombre ="Pendiente";
+        $registro2->descripcion="Registro pendiente, inconcluso, etc.";
+        $registro2->save();
 
-        $estado_registro3 = new EstadoRegistro();
-        $estado_registro3->nombre ="Revision";
-        $estado_registro3->descripcion="Registro que requiere revisión.";
-        $estado_registro3->save();
+        $registro3 = new EstadoRegistro();
+        $registro3->nombre ="Revision";
+        $registro3->descripcion="Registro que requiere revisión.";
+        $registro3->save();
 
-        $estado_registro4 = new EstadoRegistro();
-        $estado_registro4->nombre ="Duplicado";
-        $estado_registro4->descripcion="Registro duplicado en condiciones normales.";
-        $estado_registro4->save();
-
-        $estado_registro5 = new EstadoRegistro();
-        $estado_registro5->nombre ="Duplicado en Cascada";
-        $estado_registro5->descripcion="Registro duplicado en cascada. Normalmente requiere revisión";
-        $estado_registro5->save();
+        $registro4 = new EstadoRegistro();
+        $registro4->nombre ="Duplicado";
+        $registro4->descripcion="Registro duplicado en condiciones normales.";
+        $registro4->save();
+        
+        $registro5 = new EstadoRegistro();
+        $registro5->nombre ="Duplicado en Cascada";
+        $registro5->descripcion="Registro duplicado en cascada. Normalmente requiere revisión";
+        $registro5->save();
     }
 }
